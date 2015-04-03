a907226e20af4b5defdbf137f19eb54a6a1edbeb
snehal <snehaldot@gmail.com>
2015-04-03 18:54:30 +0530

    done getstream, getfollowers, getfollowing

--- python_api/app.py
+++ python_api/app.py
@@ -6,6 +6,7 @@ import logging
 from flask import request, abort, Flask, jsonify, url_for
 from functools import wraps
 import time
+from flask.ext.pymongo import PyMongo
 
 # import ujson
 # import memcache
@@ -16,7 +17,10 @@ import time
 
 
 app = Flask(__name__)
+
 # app.config['DEBUG'] = True
+# app.debug = True
+# app.run(debug=True)
 
 
 """ root or index page view functions here"""
@@ -66,6 +70,34 @@ def toJson(data):
     return json.dumps(data, default=json_util.default)
 
 
+def make_db_connection():
+    """
+        MongoDB connection
+        connection_string = 'mongodb://192.168.4.86:27017/'
+    """
+    # connection_string = 'mongodb://10.184.172.70:27017'
+    # [username, password, host, port, db] = ['livedbuser', 'WeCnI8VjEiFg4AzX3BjOg4W2R6Q8D', '54.80.251.254', '27017', 'mobapi']
+    # mongo_uri = 'mongodb://%s:%s@%s:%s/%s' % (username, password, host, port, db)
+
+    # print "mongo uri: ", mongo_uri
+    # client = MongoClient(mongo_uri)
+
+    # import ipdb
+    # ipdb.set_trace()
+    """ testing database """
+    connection_string = 'mongodb://192.168.4.86:27017/'
+    client = MongoClient(connection_string)
+
+    # db = client['mobapi']
+    db = client['mobapi_live']
+    """ test db connection """
+    print "user count: ", db.users.count()
+    return db
+
+app.config['MONGO_URI'] = 'mongodb://192.168.4.86:27017/mobapi'
+mongo = PyMongo(app, config_prefix='MONGO')
+
+
 """==================================================================================================================
     Api call for getStream which returns json
    ==================================================================================================================
@@ -74,10 +106,8 @@ def toJson(data):
 @require_appkey
 def get_stream():
     """ storing the start of time for api"""
-    # import ipdb
-    # ipdb.set_trace()
     start_time = time.time()
-    # db = client['mobapi_live']
+    db = mongo.db
     """ import for the view fuction"""
     from ast import literal_eval
     # app.logger.warning('A warning occurred (%d apples)', 42)
@@ -97,22 +127,19 @@ def get_stream():
     query_dict = {"status": "active", "company_id": id}
     query = json.dumps(query_dict)
 
-    fields = request.args.get('fields', '{}')
+    fields = request.args.get('fields', '[]')
     limit = request.args.get('limit', '1')
     page = request.args.get('page', '1')
 
     order = request.args.get('order', 1)
-    order_dict = {order: 1}
+    order_dict = {order: -1}
     order = json.dumps(order_dict)
 
     comment = request.args.get('comment', '0')
     showpromocards = request.args.get('showpromocards', '1')
     apikey = request.args.get('apikey')
-    # import ipdb
-    # ipdb.set_trace()
-    # app.logger.info('A info (query: %s)', query)
-    # logfile = logging.getLogger('file')
     logging.debug('A info (query: %s)', query)
+
     # if query:
     #     query_dict = literal_eval(query)
     # else:
@@ -132,17 +159,14 @@ def get_stream():
     if not json_results:
         # memcache miss call
         # json_results = []
-        # import ipdb
-        # ipdb.set_trace()
-        str1 = 'getStream(' + '"' + streamtype + '"' + ',' + '"' + id + '"' + ',' + '""' + ',' + query + ',' + '"' +\
+        str1 = 'getStream(' + '"' + streamtype + '"' + ',"' + id + '",' + ' "" ' + ',' + query + ',' + '"' +\
                fields + '"' + ',' + '"' + limit + '"' + ',' + '"' + page + '"' + ',' + order + ',' + '"' + comment +\
                '"' + ',' + '"' + showpromocards + '"' + ',' + '"' + apikey + '"' + ')'
         # result = db.system_js.getStream(streamtype, id, "", query, fields, limit, page, order, comment,
         #  showpromocards, apikey)
+
         query_js = db.eval(str1)
 
-        # import ipdb
-        # ipdb.set_trace()
         result = {'Total': query_js['total'], 'status': True, 'msg': "list of streams", 'data': query_js}
         format_result = {'result': result}
         json_results = json.dumps(format_result, default=json_util.default)
@@ -154,7 +178,8 @@ def get_stream():
         # r.set(memcache_key, json_results)
     end_time = time.time()
     time_take = end_time - start_time
-    print "time take: ", time_take
+    # logging.info("time take: %s for url: %s", time_take, request.url)
+    print "time take: ", time_take  #, "for url: ", request.url
     return json_results
 
 
@@ -169,8 +194,11 @@ def get_profile():
     # logging.debug('A info (query: %s)', "query")
     # logging.info("testing")
 
+    start_time = time.time()
+    db = mongo.db
+    # db = make_db_connection()
     fields = None
-    qs_result = None
+    result_qs = None
     apikey = request.args.get('apikey', None)
     #     # error codes need to be defined here
     if apikey == 'api2014mobilearcgate':
@@ -186,8 +214,6 @@ def get_profile():
     #     msg = 'Invalid apikey.'
     #     status = False
     #     final = {'result': {'status': status, 'msg': msg, 'data': None}}
-    #     # import ipdb
-    #     # ipdb.set_trace()
     #     json_results = json.dumps(final, default=json_util.default)
     #     return json_results
     #     # abort()
@@ -195,9 +221,9 @@ def get_profile():
     _id = request.args.get('_id', None)
     urlname = request.args.get('urlname', None)
     if _id:
-        qs_result = db.companies.find_one({'_id': ObjectId(_id)}, fields)
+        result_qs = db.companies.find_one({'_id': ObjectId(_id)}, fields)
     elif urlname:
-        qs_result = db.companies.find_one({'urlname': urlname}, fields)
+        result_qs = db.companies.find_one({'urlname': urlname}, fields)
     else:
         # error codes need to be defined here
         msg = 'Name OR ID required.'
@@ -207,8 +233,8 @@ def get_profile():
         return json_results
         # abort(404)
 
-    if qs_result and qs_result['_id']:
-        cid = str(qs_result['_id'])
+    if result_qs and result_qs['_id']:
+        cid = str(result_qs['_id'])
         # verify current user if verified then make status 'active', depends on user
         status = 'active'
         # check here for scope public or private, depends on user
@@ -216,20 +242,21 @@ def get_profile():
         groups = db.groups.find({'cid': cid, 'status': status, 'scope': scope}, {'_id': 1})
         groupids = [str(g['_id']) for g in groups]
         # canedit logic still figure out, depends on user
-        qs_result['_id'] = cid
-        qs_result['category'] = qs_result['category'][0]
-        qs_result['groups'] = groupids
-        qs_result['groupcount'] = len(groupids) + 1
+        result_qs['_id'] = cid
+        result_qs['category'] = result_qs['category'][0]
+        result_qs['groups'] = groupids
+        result_qs['groupcount'] = len(groupids) + 1
         status = True
         msg = "company details"
     else:
         status = False
         msg = "company doesn't exist or database issue"
-    # result = {'status': 'true', 'msg': 'successfully changed the details', 'data': qs_result}
-    # import ipdb
-    # ipdb.set_trace()
-    final = {'result': {'status': status, 'msg': msg, 'data': qs_result}}
+    # result = {'status': 'true', 'msg': 'successfully changed the details', 'data': result_qs}
+    final = {'result': {'status': status, 'msg': msg, 'data': result_qs}}
     json_results = json.dumps(final, default=json_util.default)
+    end_time = time.time()
+    time_take = end_time - start_time
+    print "time take: ", time_take, "for url: ", request.url
     return json_results
 
 
@@ -243,7 +270,7 @@ def edit_user():
     """ get the tokenId which is session id and verify with other verify function if verified moved to edit part and
      return successfully changed or unsuccessful """
 
-    print "apikey verified"
+    db = mongo.db
     about = request.args.get('about', None)
     displayname = request.args.get('displayname', None)
     username = request.args.get('username', None)
@@ -278,7 +305,7 @@ def edit_user():
 
 
 """==================================================================================================================
-    API FUNCTION TO GET COMPANY DETAILS BY PARAMETERS
+    API FUNCTION TO GET COMPANY DETAILS BY PARAMETERS  (must refactor for improving the code)
    ==================================================================================================================
 """
 @app.route('/api/company/get.json', methods=['GET'])
@@ -286,6 +313,7 @@ def edit_user():
 def get_company():
     """ get company details by params """
 
+    db = mongo.db
     name = request.args.get('name', None)
     limit_search = request.args.get('limit', 0)
     limit_search = int(limit_search) if limit_search else 0
@@ -315,8 +343,6 @@ def get_company():
         # test = db.companies.find({'name': regx})
         # check1 = [t['name'] for t in test]
         # end1 = time.time()
-        # import ipdb
-        # ipdb.set_trace()
         # mongo always runs sort and then apply the limit, with different order the limit results will be also different
         result_crx = db.companies.find({'name': {'$regex': '^' + name, "$options": "-i"}}).limit(limit_search).sort('created', orderby)
         # result_crx = db.companies.find({'name': {'$regex': '^' + name, "$options": "-i"}}).skip(skip_records)\
@@ -327,13 +353,15 @@ def get_company():
         #     data[count] = {'Company': each}
         #     count += 1
 
-    if urlname:
+    elif urlname:
         result_crx = db.companies.find({'name': {'$regex': '^' + name, "$options": "-i"}}).skip(skip_records)\
             .limit(limit_search).sort('created', orderby)
 
-        # data = list(test)
+    else:
+        result1 = {'result': {'status': False, 'msg': 'list of companies', 'Total': None, 'data': None}}
+        json_results = json.dumps(result1, default=json_util.default)
+        return json_results
 
-    # print "time: ", (end2 - start_time), "check: ", data
     for each in result_crx:
         data[count] = {'Company': each}
         count += 1
@@ -348,8 +376,6 @@ def get_company():
     # log it in logger with the time taken:
     print "api finished working with time(secs) json: ", (end_time - start_time)
     # print "api finished working with time(secs): ujson", (end_time - end_time1)
-    # import ipdb
-    # ipdb.set_trace()
 
     return json_results
 
@@ -362,6 +388,10 @@ def get_company():
 @require_appkey
 def get_fans():
     """ get fans for companies """
+
+    db = mongo.db
+    # import ipdb
+    # ipdb.set_trace()
     page = request.args.get('page', 1)
     limit = int(request.args.get('limit', 1))
     _id = request.args.get('_id', None)
@@ -381,8 +411,6 @@ def get_fans():
     # count the no. of compaines user follwoing
     # count the followers user has
 
-    # new_fan_dict = fans
-
     for each in fans:
         each['companycount'] = len(each.get('companiesfan')) if each.get('companiesfan') else 0
         each['followerscount'] = len(each.get('followers')) if each.get('followers') else 0
@@ -395,8 +423,6 @@ def get_fans():
         # *** find current user is fan of the each (user) here ???
 
         cureent_userId = None
-        # import ipdb
-        # ipdb.set_trace()
 
         # if cureent_userId in each.get('followers'):
         #     each['isFollow'] = True
@@ -412,9 +438,160 @@ def get_fans():
     # log it in logger with the time taken:
     logging.debug("api finished working with time(secs): json %s", (end_time - start_time))
     # print "api finished working with time(secs): json", (end_time - start_time)
+
+    return json_results
+
+
+"""==================================================================================================================
+    Api for get group Stream
+   ==================================================================================================================
+"""
+@app.route('/api/group/get.json', methods=['GET'])
+@require_appkey
+def get_groupstream():
+    """ get stream for group """
+
+    db = mongo.db
+    # db = make_db_connection()
+    member_id = request.args.get("members_uid")
+    members_role = [request.args.get("members_role[0]"), request.args.get("members_role[1]")]
+    admindetails = request.args.get("admindetails")
+    memberdetails = request.args.get("memberdetails")
+
+    page = request.args.get("page")
+    limit = request.args.get("limit")
+    apikey = request.args.get("apikey")
+
+    crx_groupList = db.groups.find({"members.uid": member_id,
+                                    "members": {"$elemMatch":
+                                                    {
+                                                        "uid": member_id, "role": {"$in": ["fan", "admin"]}
+                                                    }
+                                                },
+                                    "scope": {"$in": [
+                                                        "open",
+                                                        "public",
+                                                        # "private"
+                                                        ]},
+                                    })
+    # crx_groupList = db.groups.find({"$or": [
+    #                                         {"members.uid": member_id, "scope": {"$in": ["open", "public", "private"]}},
+    #                                         # {"scope": {"$in": ["open", "public"]}}
+    #                                        ]
+    #                                })
+    groupList = {}
+    count = 0
+    for group in crx_groupList:
+        # group["_id"] = str(group["_id"])
+
+        # members = db.users.find( {"_id":{"$in":[group["members"]] }})
+        members = db.users.find({"_id": {"$in": [ObjectId(x["uid"]) for x in group["members"]]}}, ["username","fname","lname","picture","email"])
+        member_dict = {x['_id']: x for x in members}
+        for member in group["members"]:
+            member["User"] = member_dict[ObjectId(member["uid"])]
+            # import ipdb
+            # ipdb.set_trace()
+
+        # group["members"] = members_dict
+        groupList[count] = {"Group": group}
+        count += 1
+
+
+        # for member in group["members"]:
+        #     user = {}
+        #     member_data = db.users.find_one({"_id": ObjectId(member['uid'])}, {"_id", "email", "picture", "username", "fname", "lname"})
+        #     # print member_data
+        #
+        #     # if not member_data:
+        #     #     none_count += 1
+        #     #     continue
+        #         # import ipdb
+        #         # ipdb.set_trace()
+        #
+        #     user["_id"] = str(member_data.get("_id"))
+        #     user["email"] = member_data.get("email", None)
+        #     user["picture"] = member_data.get("picture", None)
+        #     user["username"] = member_data.get("username", None)
+        #     user["fname"] = member_data.get("fname", None)
+        #     user["lname"] = member_data.get("lname", None)
+        #
+        #     member["User"] = user
+        #     if group["uid"] == member["uid"]:
+        #         group["createdby"] = {"User": member}
+
+
+    result = {"status": True, "msg": "list of groups", "data": groupList, "Total": crx_groupList.count()}
+    format_result = {"result": result}
+    # import ipdb
+    # ipdb.set_trace()
+    json_results = json.dumps(format_result, default=json_util.default)
+    return json_results
+
+
+"""==================================================================================================================
+    Api for get followers for users
+   ==================================================================================================================
+"""
+@app.route('/api/users/getFollowers.json', methods=['GET'])
+@require_appkey
+def getFollowers():
+    """ get followers for users """
+
+    db = mongo.db
+
+    usernname = request.args.get("username")
+    page = int(request.args.get("page", 1))
+    limit = int(request.args.get("limit", 8))
+    apikey = request.args.get("apikey")
+
+    user_id_dict = db.users.find_one({"username": usernname}, {"_id": 1})
+    user_id = user_id_dict["_id"]
+    crx_follower = db.users.find({"follow.uid": str(user_id)}, ["username", "fname", "lname", "picture", "followers",
+                                                                "companiesfan", "status"]).limit(limit)
     # import ipdb
     # ipdb.set_trace()
 
+    user_count = 0
+    data = {}
+    for user in crx_follower:
+        data[user_count] = {"User": user}
+        user_count +=1
+        # import ipdb
+        # ipdb.set_trace()
+
+    result = {"status": True, "msg": "list of followers", "data": data, "Total": crx_follower.count()}
+    format_result = {"result": result}
+    json_results = json.dumps(format_result, default=json_util.default)
+    return json_results
+
+
+"""==================================================================================================================
+    Api for get following for users and company
+   ==================================================================================================================
+"""
+@app.route('/api/users/getFollowinguserscompany.json', methods=['GET'])
+@require_appkey
+def get_followingusercompany():
+    """ get followers for users """
+
+    db = mongo.db
+
+    usernname = request.args.get("username")
+    _id = request.args.get("id")
+
+    page = int(request.args.get("page", 1))
+    limit = int(request.args.get("limit", 8))
+    apikey = request.args.get("apikey")
+
+    # import ipdb
+    # ipdb.set_trace()
+    # db.eval('getfollowing("53863567f1989d196402b2db","","5")');
+    str1 = 'getfollowing("' + _id + '"' + ',"","' + unicode(limit) + '");'
+    query_js = db.eval(str1)
+
+    result = {"status": True, "msg": "list of following", "data": query_js}
+    format_result = {"result": result}
+    json_results = json.dumps(format_result, default=json_util.default)
     return json_results
 
 
@@ -425,30 +602,15 @@ def get_fans():
 
 
 if __name__ == '__main__':
-    """
-        MongoDB connection
-        connection_string = 'mongodb://192.168.4.86:27017/'
-    """
-    connection_string = 'mongodb://10.184.172.70:27017'
-    [username, password, host, port, db] = ['livedbuser', 'WeCnI8VjEiFg4AzX3BjOg4W2R6Q8D', '54.80.251.254', '27017', 'mobapi']
-    mongo_uri = 'mongodb://%s:%s@%s:%s/%s' % (username, password, host, port, db)
 
-    print "mongo uri: ", mongo_uri
-    client = MongoClient(mongo_uri)
+    # logging.basicConfig(filename='flask_app.log', level=logging.DEBUG)
 
-    # import ipdb
-    # ipdb.set_trace()
-    """ testing database """
-    connection_string = 'mongodb://192.168.4.86:27017/'
-    client = MongoClient(connection_string)
-
-    db = client['mobapi']
-    """ test db connection """
-    # print "user count: ", db.users.count()
-
-    logging.basicConfig(filename='flask_app.log', level=logging.DEBUG)
     # handler = RotatingFileHandler('/home/www/logs/flask_app.log', maxBytes=10000, backupCount=1)
     # handler.setLevel(logging.DEBUG)
     # app.logger.addHandler(handler)
+    # app.debug = True
+
+    # app.run(debug=True, host='127.0.0.1', port=5500)
+    # app.run(host='127.0.0.1', port=5500)
+    # app.run(debug=True)
     app.run()
-    # app.test_client()
